b4f33f7248012ef43eba0e29d6ac8ce6ff2090b2
Manon <mandu10@live.fr>
2019-07-15 16:34:52 +0200

    first commit

---
+++ assets/js/scripts.js
@@ -0,0 +1,84 @@
+$(document).ready(function(){
+
+    // REALISATIONS GRAPHIQUES
+    $( "#outer" ).mouseenter(function() {
+        $("#madiv").show();
+        $("#image").hide();
+    });
+    $( "#outer" ).mouseleave(function() {
+        $("#madiv").hide();
+        $("#image").show();
+    });
+
+
+    $( "#outer1" ).mouseenter(function() {
+        $("#madiv1").show();
+        $("#image1").hide();
+    });
+    $( "#outer1" ).mouseleave(function() {
+        $("#madiv1").hide();
+        $("#image1").show();
+    });
+    $( "#outer2" ).mouseenter(function() {
+        $("#madiv2").show();
+        $("#image2").hide();
+    });
+    $( "#outer2" ).mouseleave(function() {
+        $("#madiv2").hide();
+        $("#image2").show();
+    });
+    $( "#outer3" ).mouseenter(function() {
+        $("#madiv3").show();
+        $("#image3").hide();
+    });
+    $( "#outer3" ).mouseleave(function() {
+        $("#madiv3").hide();
+        $("#image3").show();
+    });
+    $( "#outer4" ).mouseenter(function() {
+        $("#madiv4").show();
+        $("#image4").hide();
+    });
+    $( "#outer4" ).mouseleave(function() {
+        $("#madiv4").hide();
+        $("#image4").show();
+    });
+    $( "#outer5" ).mouseenter(function() {
+        $("#madiv5").show();
+        $("#image5").hide();
+    });
+    $( "#outer5" ).mouseleave(function() {
+        $("#madiv5").hide();
+        $("#image5").show();
+    });
+    $( "#outer6" ).mouseenter(function() {
+        $("#madiv6").show();
+        $("#image6").hide();
+    });
+    $( "#outer6" ).mouseleave(function() {
+        $("#madiv6").hide();
+        $("#image6").show();
+    });
+    $( "#outer7" ).mouseenter(function() {
+        $("#madiv7").show();
+        $("#image7").hide();
+    });
+    $( "#outer7" ).mouseleave(function() {
+        $("#madiv7").hide();
+        $("#image7").show();
+    });
+
+
+    // CHANGEMENT DE TAILLE
+        //A PARTIR DE ... ON CACHE LA VUE PORTABLE ET ON APPARAIT LA VUE TABLETTE/ORDI
+    // $("#portable"). width(function(){
+    //     $("#tablette").show();
+    //     $("#portable").hide();
+    // });
+        //A PARTIR DE ... ON CACHE LA VUE TABLETTE/ORDI ET ON APPPARAIT LA VUE PORTABLE
+        // $("#tablette"). width(function(){
+        //     $("#portable").show();
+        //     $("#tablette").hide();
+        // });
+});
+
